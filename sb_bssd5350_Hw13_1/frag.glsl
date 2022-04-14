da4f635ee65d56394b56b4d009dfff96095b3c7f
scottbing <bingscott@gmail.com>
2022-04-13 19:26:18 -0600

    sb_bssd5350_Hw13_1
    sb_bssd5350_Hw13_1

--- sb_bssd5350_Hw13_1/frag.glsl
+++ sb_bssd5350_Hw13_1/frag.glsl
@@ -33,5 +33,5 @@ void main(void) {
   d=r/350.0;
   d+=sin(d*d*8.0)*0.52;
   f=(sin(a*g)+1.0)/2.0;
-  gl_FragColor=vec4(vec3((f*i/1.6,i/2.0+d/13.0,i)*d*p.x+vec3(i/1.3+d/8.0,i/2.0+d/18.0,i)*d*(1.0-p.x)),1.0);
+  gl_FragColor=vec4(vec3(f*i/1.6,i/2.0+d/13.0,i)*d*p.x+vec3(i/1.3+d/8.0,i/2.0+d/18.0,i)*d*(1.0-p.x),1.0);
 }
